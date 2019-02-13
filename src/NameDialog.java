140b348a9825bcb0c44cc529d52875296dbc383e
PicoTech <PicoTech@DESKTOP-5Q8SVN6>
2019-02-13 21:04:16 +0300

    hello

---
+++ src/NameDialog.java
@@ -0,0 +1,70 @@
+import javax.swing.JOptionPane;
+public class NameDialog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = JOptionPane.showInputDialog("what is your name ? ");
+		String message = String.format("welcome , %s, to java programme", name);
+JOptionPane.showMessageDialog(null,message);
+	}
+
+}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
